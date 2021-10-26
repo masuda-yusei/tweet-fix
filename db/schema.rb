582a7410e46552437e6bf60d7cbf36479b3c3387
yusei masuda <masuda.no@icloud.com>
2021-10-26 12:24:01 +0900

    テーブル名修正

---
+++ db/schema.rb
@@ -0,0 +1,11 @@
+ActiveRecord::Schema.define(version: 2021_10_25_143537) do
+
+  enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
